244e683486061cf37dfe0020cc00488974d947b5
joadson7 <joadsontorres6@gmail.com>
2025-02-18 13:03:53 -0300

    18-02-2025

--- src/app/components/pages/cadastro-produtos/cadastro-produtos.component.ts
+++ src/app/components/pages/cadastro-produtos/cadastro-produtos.component.ts
@@ -1,11 +1,78 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-produtos',
-  imports: [],
+  imports: [
+    CommonModule,  //biblioteca de componentes comuns
+    FormsModule, //biblioteca de formulario 
+    ReactiveFormsModule //biblioteca de formularios reativos
+  ],
   templateUrl: './cadastro-produtos.component.html',
   styleUrl: './cadastro-produtos.component.css'
 })
 export class CadastroProdutosComponent {
+
+  //ATRIBUTOS
+  categorias: any[] = [];  // array de objetos json
+  mensagem: string = '';
+
+  //construtor
+  constructor(private http: HttpClient){}
+
+
+  //funcao executada ao iniciar componente
+  ngOnInit(){
+    //fazendo uma requisicao get para a API
+    this.http.get('http://localhost:8080/api/categorias/consultar')
+    .subscribe({  //aguardando retorno da API
+      next:(data) =>{ //CAPTURANDO DADOS OBTIDOS
+       //atribuindo os dados obtidos a variavel categorias
+       this.categorias = data as any[];
+      }
+    })
+  }
+
+
+  form = new FormGroup({
+    nome : new FormControl('',[
+      Validators.required, // campo obrigatorio
+      Validators.minLength(8), Validators.maxLength(100)
+    ]),
+    preco : new FormControl('', [
+      Validators.required, 
+      Validators.min(0.01)
+    ]),
+    quantidade : new FormControl('', [
+      Validators.required, Validators.min(0)
+    ]),
+    categoriaId : new FormControl('',[
+      Validators.required
+    ])
+});
+
+//funcao auxiliar se cada campo foi prenchido corretamente
+get f(){
+  return this.form.controls;
+}
+
+//funcao para capturar o evento SUBMIT do formulario
+onSubmit(){
+ //fazendo uma requisicao POST para o endpoint de cadastro de produtos
+ this.http.post('http://localhost:8080/api/produtos/cadastrar',this.form.value)
+ .subscribe({ // aguardando o retorno da API
+  next:(data: any) =>{ //capturando os dados obtidos
+    this.mensagem = data.message; // capturando mensagem de retorno
+    
+    console.log(data);
+    this.form.reset();
+  } 
+  
+ })
+
+ 
+}
 
 }

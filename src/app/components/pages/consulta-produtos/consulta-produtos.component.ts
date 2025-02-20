b9eb5cb8bd09ea24f6815e00ac6d76c3b5390e3c
joadson7 <joadsontorres6@gmail.com>
2025-02-20 13:01:46 -0300

    20-02-2025

--- src/app/components/pages/consulta-produtos/consulta-produtos.component.ts
+++ src/app/components/pages/consulta-produtos/consulta-produtos.component.ts
@@ -1,11 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-consulta-produtos',
-  imports: [],
+  imports: [
+    CommonModule, //diretivas básicas de componente do angular
+    FormsModule, //diretivas de formulário do angular
+    ReactiveFormsModule //diretivas de formulário reativo do angular
+  ],
   templateUrl: './consulta-produtos.component.html',
   styleUrl: './consulta-produtos.component.css'
 })
 export class ConsultaProdutosComponent {
+
+produtos: any[] = [];
+mensagem: string = '';
+
+  constructor(private http:HttpClient ){}
+
+  //criando o formulário
+  form = new FormGroup({
+    nome : new FormControl('', [Validators.required, Validators.minLength(3)]),
+  });  
+
+  //metodo para verificar se os campos estáo com erro
+  get f(){
+    return this.form.controls;
+  }
+
+  //metodo para enviar o formulario
+  onSubmit(){
+    this.http.get('http://localhost:8080/api/produtos/consultar/' + this.form.value.nome)
+    .subscribe({
+      next: (data) =>{
+        this.produtos = data as any[];
+        this.mensagem = `Quantidade de registros  ${this.produtos.length}`;
+        console.log(data);  
+      }
+    })
+  }
 
 }
